ab60aafc5a771c221a7411a918d7f07fe574bb9f
Chokss54 <joelyang54@gmail.com>
2024-09-05 01:03:49 +1000

    minor updates

--- src/main.py
+++ src/main.py
@@ -6,24 +6,26 @@ from config import sender_email, receiver_emails
 
 @functions_framework.http
 def snapperNOW(request):
-    url = "http://www.baywx.com.au/temps.html"
+  url = "http://www.baywx.com.au/temps.html"
 
-    # Retrieve scraped data from web
-    info = get_bay_temps(url)
+  # Retrieve scraped data from web
+  info = get_bay_temps(url)
 
-    if info:
-      warmer_bay = get_warmer_bay(info)
-      if warmer_bay:
-        sender_password = get_secret()
+  if info:
+    warmer_bay = get_warmer_bay(info)
+    if warmer_bay:
+      sender_password = get_secret()
 
-        # Load the HTML template from the file
-        with open("src/email_template.html", "r") as file:
-          html_content = file.read()
+      # Load the HTML template from the file
+      with open("email_template.html", "r") as file:
+        html_content = file.read()
 
-          # Handle send email
-          send_email(sender_email, sender_password, receiver_emails, html_content)
-          return "Snapper season"
+      # Handle send email
+      send_email(sender_email, sender_password, receiver_emails, html_content)
+      return "Snapper season NOW"    
+    return "Not snapper season yet, hang in there mate!"
         
+
 def get_warmer_bay(info):
   warmer_bay = []
   if info["Bottom Bay"] >= info["Ocean"]:
